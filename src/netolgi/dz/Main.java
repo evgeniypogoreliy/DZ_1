f58bf2d2a6d55b867ee04522ad9b76b8d7c32de6
Evgeny <joy08@mail.ru>
2022-06-02 13:44:29 +1100

    Lesson_01

---
+++ src/netolgi/dz/Main.java
@@ -0,0 +1,50 @@
+package netolgi.dz;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static String word = "javalove";
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Угадай слово");
+        int lengthWord = word.length();
+        String maskWord = "-".repeat(lengthWord);
+        System.out.println(maskWord);
+
+        do{
+            System.out.println("Введите букву");
+            char c = input.next().charAt(0);
+            if(word.indexOf(c) >= 0){
+                System.out.println("Удача");
+                for (char elem : word.toCharArray()){
+                    if(elem == c){
+                        maskWord = replaceMaskLetter(c, maskWord);
+                    }
+                }
+                System.out.println(maskWord);
+            }else {
+                System.out.println("Промах, пробуй еще раз");
+                System.out.println(maskWord);
+            }
+        }while (maskWord.contains("-"));
+        System.out.println("Поздравляем ты выйграл");
+
+    }
+    public static String replaceMaskLetter(char c, String maskWord){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == c) {
+                stringBuilder.append(c);
+            } else if (maskWord.charAt(i) != '-') {
+                stringBuilder.append(maskWord.charAt(i));
+            } else {
+                stringBuilder.append("-");
+            }
+        }
+        return stringBuilder.toString();
+
+    }
+}
